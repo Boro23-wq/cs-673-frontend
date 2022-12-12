99aec409399980d74754088d00d1036cd31bdf97
Sintu Boro <sintuboro@Sintus-MacBook-Pro.local>
2022-12-12 00:33:40 -0500

    feat: modified account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,9 +1,10 @@
 import { Layout } from '@/components/Layout'
 import { NextHead } from '@/components/NextHead'
-import { User } from 'database'
+import { Case, User } from 'database'
 import { Avatar, Button, Card, Spinner } from 'flowbite-react'
 import fetchJson from 'lib/fetchJson'
 import moment from 'moment'
+import Link from 'next/link'
 import { Upload, UploadSimple } from 'phosphor-react'
 import { useState } from 'react'
 import useSWR from 'swr'
@@ -14,6 +15,7 @@ const Account = () => {
   const [selectedFile, setSelectedFile] = useState<any>({})
 
   const { data: user } = useSWR<User>(`/api/profile`, fetchJson)
+  const { data: cases } = useSWR<Case[]>(`/api/casemanagers/cases`, fetchJson)
 
   const showUpload = () => {
     setToggleUpload(!toggleUpload)
@@ -47,15 +49,15 @@ const Account = () => {
         Personal Account
       </h1>
 
-      <div className="max-w-sm mt-4">
-        <Card>
-          <div className="flex items-center justify-between">
+      <div className="mt-4 flex flex-col md:flex-row">
+        <Card className="h-fit sm:mr-0 md:mr-4 lg:mr-4 min-w-fit">
+          <div className=" items-center justify-between">
             <h5 className="text-xl font-bold leading-none text-gray-900">
               User Information
             </h5>
           </div>
-          <div className="flow-root">
-            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          <div>
+            <ul className="divide-y divide-gray-200">
               <li className="py-1 sm:py-4">
                 {user ? (
                   <div className="flex items-center space-x-4">
@@ -84,7 +86,7 @@ const Account = () => {
                     </div>
                     <div className="min-w-0 flex-1">
                       <p className="truncate text-sm font-medium text-gray-900">
-                        {user?.firstName + '' + user?.lastName}
+                        {user?.firstName + ' ' + user?.lastName}
                       </p>
                       <p className="truncate text-sm text-gray-500">
                         {user?.email}
@@ -140,7 +142,61 @@ const Account = () => {
             </>
           )}
         </Card>
+        <Card className="bg-white rounded-md w-full mt-4 md:mt-0">
+          <h4 className="text-xl text-gray-900 font-bold">
+            Contact Information
+          </h4>
+          <ul className=" text-gray-700">
+            <li className="flex border-b py-2 border-gray-100">
+              <span className="font-bold w-24">Joined:</span>
+              <span className="text-gray-500">
+                {moment(user?.createdAt, 'YYYYMMDD').fromNow()}
+              </span>
+            </li>
+            <li className="flex border-b border-gray-100 py-2">
+              <span className="font-bold w-24">Mobile:</span>
+              <span className="text-gray-500">{user?.phone}</span>
+            </li>
+            <li className="flex py-2">
+              <span className="font-bold w-24">Email:</span>
+              <span className="text-gray-500">{user?.email}</span>
+            </li>
+          </ul>
+        </Card>
       </div>
+
+      <Card className="bg-white rounded-md mt-4">
+        <h4 className="text-xl text-gray-900 font-bold">Cases log</h4>
+        <div className="relative px-4">
+          <div className="absolute h-full border border-dashed border-opacity-20 border-secondary"></div>
+
+          {cases?.slice(0, 4).map((patientCase, index) => (
+            <div key={index} className="flex items-center w-full my-6 -ml-1.5">
+              <div className="w-1/12 z-10">
+                <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
+              </div>
+              <div className="w-11/12">
+                <p className="text-sm">
+                  Case{' '}
+                  <a href="#" className="text-blue-600 font-bold">
+                    #{patientCase.id}
+                  </a>{' '}
+                  was created. It is considered{' '}
+                  {patientCase?.severityLevel.split('-')[1].toLowerCase()}. The
+                  case is under the {patientCase?.categoryTitle.toLowerCase()}{' '}
+                  (top-level) category.
+                </p>
+                <p className="text-xs text-gray-500">
+                  {moment(user?.createdAt, 'YYYYMMDD').fromNow()}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Link className="text-gray-500 invisible sm:visible" href="/cases">
+          View all cases &rarr;
+        </Link>
+      </Card>
     </Layout>
   )
 }
